78e2184e4dde63b54889c804c43266ecef90b25f
capitalhealthcaree <premierpatientappointment@gmail.com>
2024-01-10 09:52:09 -0800

    banner added

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ import Contact from "@/components/sections/home5/Contact";
 import Request from "@/components/sections/home5/Request";
 import MedicalProcedures from "@/components/sections/home5/MedicalProcedures";
 import Slider from "@/components/sections/home5/Slider";
+import Banner from "@/components/sections/home3/Banner"
 
 export default function Home() {
   return (
     <>
-      <Layout headerStyle={5}>
-        <Slider />
+      <Layout headerStyle={6}>
+        <Banner />
         <MedicalProcedures />
         <AboutTwo />
         <Request />
