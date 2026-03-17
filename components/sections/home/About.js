966a3d35aa18b4a75ea62bdc438832bcd660585c
capitalhealthcaree <premierpatientappointment@gmail.com>
2026-03-18 01:49:32 +0500

    all capitilize issue fixed

--- components/sections/home/About.js
+++ components/sections/home/About.js
@@ -26,44 +26,42 @@ export default function About() {
               </div>
             </div>
             <div className="col-lg-6">
-              <div className="about-content-seven">
-                <div className="section-title mb-30">
-                  <span className="sub-title">About Us</span>
-                  <h2
-                    className="title"
-                    style={{ color: "black", fontSize: "39px" }}
-                  >
-                    Excellence in Advanced Surgery Center and Pain Management
-                  </h2>
-                </div>
-                <p style={{ color: "black", fontSize: "16px" }}>
-                  AdvancedCare Surgery Center provides exceptional surgical care
-                  with a priority on patient safety, comfort, and well-being.
-                  AdvancedCare brings together a team of dedicated surgeons,
-                  anesthesiologists, nurses, and support staff to provide
-                  advanced medical treatments in a caring and individualized
-                  environment. We provide high-quality treatment tailored to
-                  each patient’s needs for both routine and complex surgeries.
-                  At AdvancedCare Surgery Center, we believe that excellent care
-                  should be available.
-                </p>
-
-                <p
-                  className="info-two"
-                  style={{ color: "black", fontSize: "16px" }}
+              <div className="section-title mb-30">
+                <span className="sub-title">About Us</span>
+                <h2
+                  className="title"
+                  style={{ color: "black", fontSize: "39px" }}
                 >
-                  Our pre-surgical consultations and care reflect our
-                  patient-centric approach. Our dedication to continuous
-                  progress and medical advancement enables us to provide our
-                  patients with the most advanced therapies and compassionate
-                  care, ensuring a speedy recovery and optimal results. Trust
-                  AdvancedCare Surgery Center to be your health partner, with
-                  every detail geared to improve your surgery and wellness.
-                </p>
-                <Link href="/appointment" className="btn btn-three">
-                  Schedule Appointment
-                </Link>
+                  Excellence in Advanced Surgery Center and Pain Management
+                </h2>
               </div>
+              <p style={{ color: "black", fontSize: "16px" }}>
+                AdvancedCare Surgery Center provides exceptional surgical care
+                with a priority on patient safety, comfort, and well-being.
+                AdvancedCare brings together a team of dedicated surgeons,
+                anesthesiologists, nurses, and support staff to provide advanced
+                medical treatments in a caring and individualized environment.
+                We provide high-quality treatment tailored to each patient’s
+                needs for both routine and complex surgeries. At AdvancedCare
+                Surgery Center, we believe that excellent care should be
+                available.
+              </p>
+
+              <p
+                className="info-two"
+                style={{ color: "black", fontSize: "16px" }}
+              >
+                Our pre-surgical consultations and care reflect our
+                patient-centric approach. Our dedication to continuous progress
+                and medical advancement enables us to provide our patients with
+                the most advanced therapies and compassionate care, ensuring a
+                speedy recovery and optimal results. Trust AdvancedCare Surgery
+                Center to be your health partner, with every detail geared to
+                improve your surgery and wellness.
+              </p>
+              <Link href="/appointment" className="btn btn-three">
+                Schedule Appointment
+              </Link>
             </div>
           </div>
         </div>
--- pages/about.js
+++ pages/about.js
@@ -1,83 +1,11 @@
 import Layout from "@/components/layout/Layout";
-import Link from "next/link";
+import About from "@/components/sections/home/About";
 
-export default function About() {
+export default function AboutUs() {
   return (
     <>
       <Layout breadcrumbTitle="About Us">
-        <div>
-          <section
-            className=""
-            style={{ paddingBottom: "50px", paddingTop: "50px" }}
-          >
-            <div className="container">
-              <div className="row align-items-center justify-content-center">
-                <div className="col-lg-6 col-md-9 order-0 order-lg-2">
-                  <div className="about-img-seven-wrap">
-                    <img
-                      src="/assets/img/aboutus/right-one.jpg"
-                      alt=""
-                      data-aos="fade-right"
-                      data-aos-delay={0}
-                    />
-                    <img
-                      src="/assets/img/aboutus/right-two.jpg"
-                      alt=""
-                      data-aos="fade-up"
-                      data-aos-delay={300}
-                    />
-                    <img
-                      src="/assets/img/images/inner_about_shape01.png"
-                      alt=""
-                      className="shape"
-                      data-aos="zoom-in"
-                      data-aos-delay={500}
-                    />
-                    <div
-                      className="experience-wrap"
-                      data-aos="fade-left"
-                      data-aos-delay={0}
-                    >
-                      <h2 className="title">05</h2>
-                      <p>Years Of Experience</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6">
-                  <div className="about-content-seven">
-                    <div className="section-title mb-30">
-                      <span className="sub-title">About Us</span>
-                      <h2 className="title" style={{ fontSize: "39px" }}>
-                        Compassionate Surgical Care
-                      </h2>
-                    </div>
-                    <p>
-                      At Encore Surgery Center, we prioritize your comfort and
-                      well-being. Our dedicated team offers superior care,
-                      ensuring a stress-free surgical experience. Utilizing the
-                      latest medical technology and high-standard protocols, we
-                      are committed to making your procedure successful and your
-                      experience as comfortable as possible.
-                    </p>
-
-                    <p className="info-two">
-                      Understanding the emotional impact of surgery, our
-                      compassionate staff provides unwavering support throughout
-                      your journey. Our expert physicians are devoted to your
-                      care, focusing on achieving long-term results that meet
-                      your expectations. We're honored to be trusted with your
-                      health and are dedicated to delivering outstanding care at
-                      every step.
-                    </p>
-                    <Link href="/medical-procedures/" className="btn btn-three">
-                      Our Medical Procedures
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
+        <About />
       </Layout>
     </>
   );
